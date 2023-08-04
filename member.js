aecdbb64d08d0e0126e3ae8b69854a7fef12bfcf
Kyle Sheehan <ksheehan927@gmail.com>
2023-08-04 02:03:50 +0000

    second commit. completions panel doesnt work

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "React"];
+    var member = {
+        name: "Anna",
+        age: 25,
+        skills: skills,
+    };
+    return member;
+}
